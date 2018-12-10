643304a012368c23337f567ea0bcd747a79f9105
dewey <820554415@qq.com>
2018-12-10 16:59:07 +0800

    create project MicroServer

---
+++ MicroServer/src/main/java/com/demo/MicroServer/MicroServerApplication.java
@@ -0,0 +1,12 @@
+package com.demo.MicroServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServerApplication.class, args);
+	}
+}
