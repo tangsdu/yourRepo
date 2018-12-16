b329d9e6995531f370eef497015e007d5235676b
tommy <tomsdo@126.com>
2018-12-16 13:34:41 +0800

    432432

--- MicroServer/src/main/java/com/demo/MicroServer/MicroServerApplication.java
+++ MicroServer/src/main/java/com/demo/MicroServer/MicroServerApplication.java
@@ -2,7 +2,18 @@ package com.demo.MicroServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * 
+ * 
+ * 功能：
+ * 
+ * @version 1.0
+ * @since JDK1.8
+ * @author tangs
+ * 版权所有：tangshengdu(C) 2017。
+ * 日期： 2018年12月16日 下午1:33:14
+ */
 @SpringBootApplication
 public class MicroServerApplication {
 
